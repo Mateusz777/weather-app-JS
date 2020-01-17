2114db00658c5633e3701c68a1f7c8efad181777
Mateusz Szpatusko <esz.mateusz@gmail.com>
2020-01-17 22:41:48 +0100

    add styles and part of date from  API

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+    let long; //długość geo
+    let lat;  //szerokość geo
+    const temperatureExtraInfo = document.querySelector('.temperature__extraInfo')
+    const temperatureDegrees = document.querySelector('.temperature__degrees')
+    const locationTimezone = document.querySelector('.location__timezone')
+    const location = document.querySelector('.location__visual')
+
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            // console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            // console.log(long, lat);
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const API_URL = `${proxy}https://api.darksky.net/forecast/83dac79a384a72ae030c5e84d220a22b/${lat},${long}?units=si`;
+
+            fetch(API_URL)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                const {temperature, summary} = data.currently;
+                temperatureExtraInfo.textContent = summary;
+                temperatureDegrees.textContent = temperature;
+
+            })
+        });
+    }
+});
